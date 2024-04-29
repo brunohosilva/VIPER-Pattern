b6a22ed6565ce939e8530aefcb167c3fd9279162
Bruno Oliveira <brunohosilva97@gmail.com>
2024-04-28 22:16:38 -0300

    :sparkles: Criado Factory para instanciar e iniciar a estrutura

--- VIPER/VIPER/Router.swift
+++ VIPER/VIPER/Router.swift
@@ -15,32 +15,10 @@ typealias EntryPoint = AnyView & UIViewController
 
 protocol AnyRouter {
     var entry: EntryPoint? { get }
-    static func start() -> AnyRouter
 }
 
 class UserRouter: AnyRouter {
     
     var entry: EntryPoint?
     
-    static func start() -> AnyRouter {
-        let router = UserRouter()
-        
-        // Assing VIP
-        
-        var view: AnyView = UserViewController()
-        var presenter: AnyPresenter = UserPresenter()
-        var interactor: AnyInteractor = UserInteractor()
-        
-        view.presenter = presenter
-
-        interactor.presenter = presenter
-        
-        presenter.router = router
-        presenter.view = view
-        presenter.interactor = interactor
-        
-        router.entry = view as? EntryPoint
-        
-        return router
-    }
 }
